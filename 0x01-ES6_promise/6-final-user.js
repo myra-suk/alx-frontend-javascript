cd22b87debd36eb4b85504a9cd97dea09419b0fb
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-07-01 01:08:56 +0300

    Create 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+* Write and export a function handleProfileSignup
+ * It should accept firstName, lastName, and fileName arguments
+ */
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      for (const result of results) {
+        if (result.status === 'rejected') {
+          result.value = `Error: ${result.reason ? result.reason.message : 'Unknown error'}`;
+          delete result.reason;
+        }
+      }
+      return results;
+    });
+}
+
+export default handleProfileSignup;
