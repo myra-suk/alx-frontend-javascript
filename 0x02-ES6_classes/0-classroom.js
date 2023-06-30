a62271fed0fec6e04ed3b53a3804552caee7a406
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-07-01 01:20:00 +0300

    Create 0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/* Implement a class named "Classroom" with the following properties:
+ * An attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
+ */
+
+/* eslint-disable no-underscore-dangle */
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
